67a8c958fa018cf5814b380e355df05012357051
dinosauce-285 <thanhbualqt@gmail.com>
2022-02-23 20:22:32 +0700

    lmao

---
+++ app.js
@@ -0,0 +1,15 @@
+let btn = document.getElementById("searchButton");
+let box = document.getElementById("searchBox");
+let icon = document.getElementById("searchIcon");
+function clickToShow() {
+if (box.style.width = '400px') {
+    box.style.width = '70px '
+   
+} 
+
+
+
+}
+btn.onclick = clickToShow
+
+
